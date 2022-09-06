1d3db6fb8c59a92f472a6e08decf2d19b142b77c
beomwon <38881094+beomwon@users.noreply.github.com>
2022-09-06 16:35:13 +0900

    Time: 36 ms (85.88%), Space: 13.8 MB (68.92%) - LeetHub

---
+++ 814-binary-tree-pruning/814-binary-tree-pruning.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def pruneTree(self, root):
+        
+        if not root: 
+            return None
+        
+        root.left = self.pruneTree(root.left)
+        root.right = self.pruneTree(root.right)
+        
+        if root.val:
+            return root
+        else:
+            return root if root.left or root.right else None
